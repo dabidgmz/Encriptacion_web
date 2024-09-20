a5dbc07b11d44e437dbef2f9cdbc54305e8433d8
David Herrera <david.gmzherrera28@gmail.com>
2024-09-20 15:04:20 -0600

    se agrego users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
+import { IndexUsersComponent } from './index-user/index-user.component';
+export const routes: Routes = [
+    { path: 'user_index', component: IndexUsersComponent},
+];
 
-export const routes: Routes = [];
